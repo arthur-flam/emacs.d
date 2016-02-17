3f9bfadb08ec998646a169309acb44554a9947fe
Arthur Flam <arthur.flam@gmail.com>
2016-02-17 18:19:37 +0200

    removed temp file

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("39c7a0869b479f86548bc11b5d7b32237398949fe94f9a2c23a13146f34b638d" default))))
+    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "39c7a0869b479f86548bc11b5d7b32237398949fe94f9a2c23a13146f34b638d" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
